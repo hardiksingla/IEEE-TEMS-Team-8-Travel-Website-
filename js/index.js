79a59e4b5e9b288222f3f3ad41864fd7794f57c5
hardiksingla <07hardiksingla@gmail.com>
2023-08-05 18:15:00 +0530

    popup

--- js/index.js
+++ js/index.js
@@ -1,45 +1,57 @@
 
 //Video Cards
   
-  const videoCards = [...document.querySelectorAll(".video-card")];
+const videoCards = [...document.querySelectorAll(".video-card")];
   
-  videoCards.forEach((item) => {
-    item.addEventListener("mouseover", () => {
-      let video = item.children[1];
-      video.play();
-    });
-    item.addEventListener("mouseleave", () => {
-      let video = item.children[1];
-      video.pause();
-    });
+videoCards.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    let video = item.children[1];
+    video.play();
   });
-  
-  //card sliders
-  
-  var videocardContainers = [...document.querySelectorAll(".video-card-container")];
-  var preBtns = [...document.querySelectorAll(".pre-btn")];
-  var nxtBtns = [...document.querySelectorAll(".nxt-btn")];
-  
-  videocardContainers.forEach((item, i) => {
-    var containerDimensions = item.getBoundingClientRect();
-    var containerWidth = containerDimensions.width;
-  
-    nxtBtns[i].addEventListener("click", () => {
-      item.scrollLeft += containerWidth - 200;
-    });
-  
-    preBtns[i].addEventListener("click", () => {
-      item.scrollLeft -= containerWidth + 200;
-    });
+  item.addEventListener("mouseleave", () => {
+    let video = item.children[1];
+    video.pause();
+  });
+});
+
+//card sliders
+
+var videocardContainers = [...document.querySelectorAll(".video-card-container")];
+var preBtns = [...document.querySelectorAll(".pre-btn")];
+var nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+
+videocardContainers.forEach((item, i) => {
+  var containerDimensions = item.getBoundingClientRect();
+  var containerWidth = containerDimensions.width;
+
+  nxtBtns[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth - 200;
   });
 
+  preBtns[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth + 200;
+  });
+});
 
-  
-  function togglediv(id,id1) {
-    var div = document.getElementById(id);
-    var div1 = document.getElementById(id1);
-    div.style.display = div.style.display == "block" ? "none" : "block";
-    div1.style.display = div.style.display == "flex" ? "none" : "flex";
+
+
+function togglediv(id,id1) {
+  var div = document.getElementById(id);
+  var div1 = document.getElementById(id1);
+  div.style.display = div.style.display == "block" ? "none" : "block";
+  div1.style.display = div.style.display == "flex" ? "none" : "flex";
 }
-  
-  
+
+var tryNowButton = document.getElementById("try-now-button");
+const popup = document.getElementById("popup");
+const backdrop = document.getElementById("content");
+
+console.log(popup,tryNowButton)
+
+tryNowButton.addEventListener("click", () => {
+popup.style.display = "block";
+});
+
+backdrop.addEventListener("click", () => {
+popup.style.display = "none";
+});
