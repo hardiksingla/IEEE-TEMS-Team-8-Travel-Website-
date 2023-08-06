a1c45dbe61b766ccaea965d8cdd06d29e39e0842
tarush10000 <tarushagarwal2003@gmail.com>
2023-08-07 01:31:43 +0530

    popup final i guess

--- js/default.js
+++ js/default.js
@@ -2,3 +2,11 @@ var div = document.getElementById("top-destination");
 div.style.display = "none";
 var div1 = document.getElementById("navbar");
 div1.style.display = "none";
+
+function openForm() {
+    document.getElementById("popup").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("popup").style.display = "none";
+}
