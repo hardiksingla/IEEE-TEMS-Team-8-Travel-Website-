c1494f448f267751d11d787ef39376c64c7543db
tarush10000 <tarushagarwal2003@gmail.com>
2023-08-09 22:31:02 +0530

    loading and thank you popup

--- js/default.js
+++ js/default.js
@@ -3,10 +3,30 @@ div.style.display = "none";
 var div1 = document.getElementById("navbar");
 div1.style.display = "none";
 
+//open and close popup
 function openForm() {
     document.getElementById("popup").style.display = "block";
-}
-  
+} 
 function closeForm() {
+    document.getElementById("popup").style.display = "none";
+}
+
+
+//open and close loading page
+function openloading() {
+    document.getElementById("loading-popup").style.display = "block";
+    
+    setTimeout(function() {
+        document.getElementById("loading-popup").style.display = "none"; // Close loading popup
+        document.getElementById("thank-popup").style.display = "block";  // Open thank popup
+    }, 10000);
+}
+function closeloading(){
+    document.getElementById("loading-popup").style.display = "none";
+}
+
+//close thank page
+function closethank() {
+    document.getElementById("thank-popup").style.display = "none";
     document.getElementById("popup").style.display = "none";
 }
